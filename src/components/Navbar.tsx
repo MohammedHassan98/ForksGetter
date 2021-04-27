f6be9724a8b73c7a6843c108bb2ffc71f0efbeb9
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2021-04-27 05:34:24 +0200

    Basic Page layout + structure the project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className="NavBar">
+            <ul className="navItems">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#"> Favorites </a>
+                </li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/views/MainPage.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class MainPage extends Component {
+    render() {
+        return (
+            <div className="MainDiv">
+                <div className="WelcomeSection">
+                    <h2>Welcome To Forks Getter Application </h2>
+                </div>
+                <div className="normalText">
+                    <p>
+                        {" "}
+            You Can type Repository Name at the form " owner /:repositoryName"
+            then click search and we will provide Repo's full name of the<br/>
+            repository, owner, number of stars, link to the fork repository{" "}
+                    </p>
+                </div>
+                <div className="SearchArea"></div>
+            </div>
+        );
+    }
+}
