39613dae9841063ffb882af708b1a1530ba98c80
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2021-04-27 06:08:31 +0200

    Basic Main Page styling and search bar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,8 +3,13 @@ import React, { Component } from 'react'
 export default class Search extends Component {
     render() {
         return (
-            <div>
-                Search Form
+            <div className="form-group">
+               <form>
+                   <input type="text" name="search" placeholder="Type Repo Name Here" className="form-control" />
+                   <button className="SearchButton" >
+                       Search
+                   </button>
+               </form>
             </div>
         )
     }
